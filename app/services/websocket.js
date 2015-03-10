58eb416c31fed8625f6697138cbf38717f495167
Cedric Warny <cedric.warny@gmail.com>
2015-03-09 22:24:50 -0400

    deleted a few files not needed in this branch and added some new

--- app/services/websocket.js
+++ app/services/websocket.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
-/* global io */
-
 export default Ember.Service.extend({
-	_setup: function () {
-		this.socket = io(`${window.location.hostname}:8080`);
-	}.on("init"),
-
-	send: function (message) {
-		this.socket.emit("message", message);
-	}
 });
