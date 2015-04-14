0d9e7c196c2c4367a18295d9045704e3050d6788
Cedric Warny <cedric.warny@gmail.com>
2015-04-13 21:05:14 -0400

    Added a few things to the readme

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key: "1j38Lof7ffvBor64utpEdQ",
-	consumer_secret: "d8m0ceCV3LVR1wtMf2j8AvkKActmkjnG8llNerwl6aI",
-	token: "347348265-Sz2pziQsQvUmJYAPu9At24BZPndR07Z5qsWJmTtG",
-	token_secret: "MymTefV8vFEUmHe23pKU0QTdMwLI70LGckHIJJnmA9D2y"
-};
